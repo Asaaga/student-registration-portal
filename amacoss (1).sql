7ed5d5497f6e0f1f7c8a44d030bcf7d3da5e86b0
Asaaga Simon <simonasarga@gmail.com>
2023-07-31 16:49:45 +0100

    initial commit

---
+++ amacoss (1).sql
@@ -0,0 +1,310 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 13, 2023 at 12:21 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.1.17
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `amacoss`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `admin`
+--
+
+CREATE TABLE `admin` (
+  `id` int(11) NOT NULL,
+  `username` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `admin`
+--
+
+INSERT INTO `admin` (`id`, `username`, `password`) VALUES
+(1, 'tipu', '202cb962ac59075b964b07152d234b70'),
+(2, 'admin', '202cb962ac59075b964b07152d234b70');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `attn`
+--
+
+CREATE TABLE `attn` (
+  `id` int(11) NOT NULL,
+  `st_id` int(11) NOT NULL,
+  `atten` varchar(50) NOT NULL,
+  `at_date` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `attn`
+--
+
+INSERT INTO `attn` (`id`, `st_id`, `atten`, `at_date`) VALUES
+(206, 12103072, 'absent', '2021-04-11'),
+(207, 13103072, '', '0000-00-00'),
+(208, 13203072, '', '0000-00-00'),
+(209, 14103053, '', '0000-00-00'),
+(210, 14203072, '', '0000-00-00'),
+(211, 12103072, 'absent', '2021-06-05'),
+(212, 13103072, 'absent', '2021-06-05'),
+(213, 13203072, 'absent', '2021-06-05'),
+(214, 14103053, 'present', '2021-06-05'),
+(215, 14203072, 'absent', '2021-06-05'),
+(216, 1454540, '', '0000-00-00'),
+(217, 17699619, '', '0000-00-00'),
+(218, 12103072, 'present', '2021-06-06'),
+(219, 13103072, 'present', '2021-06-06'),
+(220, 13203072, 'present', '2021-06-06'),
+(221, 14103053, 'absent', '2021-06-06'),
+(222, 14203072, 'absent', '2021-06-06'),
+(223, 1454540, 'present', '2021-06-06'),
+(224, 17699619, 'present', '2021-06-06'),
+(225, 0, 'absent', '2023-07-12'),
+(226, 0, 'absent', '2023-07-12'),
+(227, 0, 'absent', '2023-07-12'),
+(228, 0, 'absent', '2023-07-12'),
+(229, 0, 'absent', '2023-07-12'),
+(230, 0, 'absent', '2023-07-12'),
+(231, 0, 'absent', '2023-07-12'),
+(232, 0, 'absent', '2023-07-12'),
+(233, 0, 'absent', '2023-07-12'),
+(234, 0, 'absent', '2023-07-12'),
+(235, 0, 'absent', '2023-07-12');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `at_student`
+--
+
+CREATE TABLE `at_student` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `st_id` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `at_student`
+--
+
+INSERT INTO `at_student` (`id`, `name`, `st_id`) VALUES
+(1, 'Anyagh Terkuma', 'BSU/SC/CMP/17/45692'),
+(2, 'Gena Isaac', 'BSU/SC/CMP/17/45704'),
+(3, 'Godi Nguumbuur Agatha', 'BSU/SC/CMP/17/45705'),
+(4, 'Julius Oka Christiana', 'BSU/SC/CMP/17/45718'),
+(5, 'Kumaga Daniel Terkuma', 'BSU/SC/CMP/17/45721'),
+(6, 'Oche Alexander Ekoja', 'BSU/SC/CMP/17/45726'),
+(7, 'Ojih Jesse', 'BSU/SC/CMP/17/45732'),
+(8, 'Asaaga Simon', 'BSU/SC/CMP/17/45741'),
+(9, 'Tatyough Jeremiah Sughter', 'BSU/SC/CMP/17/45742'),
+(10, 'Tsav Samson Agwaza', 'BSU/SC/CMP/17/45748'),
+(11, 'Atsehe Nadoo Keziah', 'BSU/SC/CMP/17/50089');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `faculty`
+--
+
+CREATE TABLE `faculty` (
+  `id` int(11) NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(30) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `birthday` date DEFAULT NULL,
+  `gender` varchar(10) NOT NULL,
+  `education` varchar(100) DEFAULT NULL,
+  `contact` int(100) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `faculty`
+--
+
+INSERT INTO `faculty` (`id`, `username`, `password`, `name`, `email`, `birthday`, `gender`, `education`, `contact`, `address`) VALUES
+(1, 'robinson', '202cb962ac59075b964b07152d234b70', 'Terry Robinson', 'terryr@gmail.com', '1986-04-01', 'Male', 'BIT, MIT', 900248750, '3962  Elk Rd Little'),
+(13, 'will', '202cb962ac59075b964b07152d234b70', 'Will Williams', 'will@gmail.com', '1985-12-12', 'Male', 'MIT, PhD', 124785450, '3308  Valley Drive');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `result`
+--
+
+CREATE TABLE `result` (
+  `id` int(11) NOT NULL,
+  `st_id` int(11) NOT NULL,
+  `marks` int(5) NOT NULL,
+  `sub` varchar(50) NOT NULL,
+  `semester` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `result`
+--
+
+INSERT INTO `result` (`id`, `st_id`, `marks`, `sub`, `semester`) VALUES
+(28, 12103072, 95, 'DBMS', '1st'),
+(29, 12103072, 92, 'DBMS Lab', '1st'),
+(30, 12103072, 90, 'Mathematics', '1st'),
+(31, 12103072, 80, 'Programming', '1st'),
+(32, 12103072, 90, 'Programming Lab', '1st'),
+(35, 12103072, 95, 'Physics', '2nd'),
+(36, 12103072, 96, 'Mathematics', '2nd'),
+(37, 12103072, 85, 'Chemistry', '2nd'),
+(38, 12103072, 78, 'Psychology', '2nd'),
+(39, 15103058, 96, 'DBMS', '1st'),
+(40, 15103058, 98, 'DBMS Lab', '1st'),
+(41, 15103058, 65, 'Mathematics', '1st'),
+(42, 15103058, 90, 'Programming', '1st'),
+(43, 15103058, 92, 'Programming Lab', '1st'),
+(44, 15103058, 95, 'English', '2nd'),
+(45, 15103058, 50, 'Physics', '2nd'),
+(46, 15103058, 68, 'Chemistry', '2nd'),
+(47, 15103058, 92, 'Psychology', '2nd'),
+(48, 15103157, 89, 'DBMS', '1st'),
+(49, 15103157, 90, 'DBMS Lab', '1st'),
+(50, 15103157, 80, 'Mathematics', '1st'),
+(51, 15103157, 95, 'English', '1st'),
+(52, 16303053, 85, 'DBMS', '1st'),
+(53, 15103053, 75, 'DBMS Lab', '1st'),
+(54, 15103053, 40, 'Mathematics', '1st'),
+(55, 15103053, 85, 'Programming', '1st'),
+(56, 15103053, 90, 'Programming Lab', '1st'),
+(57, 15103053, 87, 'English', '1st'),
+(58, 15103053, 60, 'Physics', '1st'),
+(59, 15103053, 65, 'Chemistry', '1st'),
+(60, 15103053, 69, 'Psychology', '1st'),
+(61, 17699619, 71, 'DBMS', '1st'),
+(62, 17699619, 82, 'DBMS Lab', '1st'),
+(63, 17699619, 65, 'Mathematics', '1st'),
+(64, 17699619, 85, 'Programming', '1st'),
+(65, 17699619, 86, 'Programming Lab', '1st'),
+(66, 17699619, 87, 'English', '1st'),
+(67, 17699619, 70, 'Physics', '1st'),
+(68, 17699619, 75, 'Chemistry', '1st'),
+(69, 17699619, 70, 'Psychology', '1st'),
+(70, 0, 76, 'Programming Lab', '1st');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `st_info`
+--
+
+CREATE TABLE `st_info` (
+  `st_id` varchar(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `bday` date NOT NULL,
+  `program` varchar(10) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `gender` varchar(10) NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `img` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Dumping data for table `st_info`
+--
+
+INSERT INTO `st_info` (`st_id`, `name`, `password`, `email`, `bday`, `program`, `contact`, `gender`, `address`, `img`) VALUES
+('bsu/sc/cmp/17/45681', 'Abah Nicholas ', '202cb962ac59075b964b07152d234b70', 'abahnicholas1996@gmail.com', '1999-08-20', 'Bsc. Compu', '08137987048', 'Male', 'Otukpo benue State', NULL),
+('bsu/sc/cmp/17/45723', 'Jafaru Mohammed Ibrahim', '202cb962ac59075b964b07152d234b70', 'jimjafaru@gmail.com', '1995-02-25', 'Bsc. Compu', '08056784449', 'Male', 'wadata Makurdi', NULL),
+('bsu/sc/cmp/17/45742', 'tatyough Jeremiah Sughter', '202cb962ac59075b964b07152d234b70', 'jstatyough02@gmail.com', '1996-10-17', 'Bsc. Compu', '09154149467', 'Male', 'North bank Makurdi', NULL);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `admin`
+--
+ALTER TABLE `admin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `attn`
+--
+ALTER TABLE `attn`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `at_student`
+--
+ALTER TABLE `at_student`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `faculty`
+--
+ALTER TABLE `faculty`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- Indexes for table `result`
+--
+ALTER TABLE `result`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `st_info`
+--
+ALTER TABLE `st_info`
+  ADD PRIMARY KEY (`st_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `attn`
+--
+ALTER TABLE `attn`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=236;
+
+--
+-- AUTO_INCREMENT for table `at_student`
+--
+ALTER TABLE `at_student`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+
+--
+-- AUTO_INCREMENT for table `faculty`
+--
+ALTER TABLE `faculty`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `result`
+--
+ALTER TABLE `result`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
